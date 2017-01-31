3dd591484cb00c4327ec08ac46fb43832c96c778
Walter Sobral <wsa@cin.ufpe.br>
2017-01-31 13:12:37 -0300

    Added dependencies and license to setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='kafka-paperplane',
       version='0.1',
       description='API for Apache Kafka as a notification system.',
       author='Walter Sobral',
       author_email='wsacin@gmail.com',
+      license='apache-2.0',
       packages=[
           'kafka_paperplane',
           'kafka_paperplane.persistence',
+      ],
+      install_requires=[
+          'yajl'
+          'kafka-python',
+          'pymongo',
+          'redis',
       ]
       )
